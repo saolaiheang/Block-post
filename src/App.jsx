82bc4eae9ccd50ed5bda4a75b527ef94dbdf51b5
saolaiheang <laiheang.sao@institute.pse.ngo>
2024-10-07 16:59:08 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
 import NotFound from './components/Notfound';
-
+import PrivateRoute from './components/PrivateRoute';
+import Blogmodule from './pages/Blogmodule';
+import Login from './pages/Login';
 
 function App() {
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/blogmodule"
+          element={
+            <PrivateRoute>
+              <Blogmodule />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
